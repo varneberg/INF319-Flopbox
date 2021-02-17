084cf6029970d993c9f606639e487183702c35b9
kristoffer <kristofferbn98@gmail.com>
2021-02-17 15:02:56 +0100

    cant get path to file working

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -5,6 +5,9 @@ public class Client implements Runnable{
     Thread t;
     String message;
     int port;
+    private static DataOutputStream dataOutputStream = null;
+    private static DataInputStream dataInputStream = null;
+
 
     public Client(int port) {
         this.port = port;
@@ -14,15 +17,37 @@ public class Client implements Runnable{
     public void run(){
         try{
             Socket s = new Socket("localhost",port);
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            dout.writeUTF("Hello Server");
-            dout.flush();
-            dout.close();
+            dataInputStream = new DataInputStream(s.getInputStream());
+            dataOutputStream = new DataOutputStream(s.getOutputStream());
+
+            sendFile("/clientStorage/send1.txt");
+            sendFile("/clientStorage/send2.txt");
+
+            dataInputStream.close();
+            dataInputStream.close();
             s.close();
         }catch(Exception e){
             System.out.println(e);
         }
     }
+
+    private void sendFile(String path) throws Exception{
+        int bytes = 0;
+        File file = new File(path);
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        // send file size
+        dataOutputStream.writeLong(file.length());
+        // break file into chunks
+        byte[] buffer = new byte[4*1024];
+        while ((bytes=fileInputStream.read(buffer))!=-1){
+            dataOutputStream.write(buffer,0,bytes);
+            dataOutputStream.flush();
+        }
+        fileInputStream.close();
+
+    }
+
 
     public String getMessage() {
         return message;
@@ -36,3 +61,48 @@ public class Client implements Runnable{
         return port;
     }
 }
+
+
+
+/*
+
+
+// testkode
+import java.io.*;
+        import java.net.Socket;
+
+public class Client {
+    private static DataOutputStream dataOutputStream = null;
+    private static DataInputStream dataInputStream = null;
+
+    public static void main(String[] args) {
+        try(Socket socket = new Socket("localhost",5000)) {
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            sendFile("../../../storage/clientStorage/send1.txt");
+            sendFile("../../../storage/clientStorage/send2.txt");
+
+            dataInputStream.close();
+            dataInputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendFile(String path) throws Exception{
+        int bytes = 0;
+        File file = new File(path);
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        // send file size
+        dataOutputStream.writeLong(file.length());
+        // break file into chunks
+        byte[] buffer = new byte[4*1024];
+        while ((bytes=fileInputStream.read(buffer))!=-1){
+            dataOutputStream.write(buffer,0,bytes);
+            dataOutputStream.flush();
+        }
+        fileInputStream.close();
+    }
+}*/
