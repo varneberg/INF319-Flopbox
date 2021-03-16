1cba4d206c4581f90099b378cb9fe1345127ac87
kristoffer <kristofferbn98@gmail.com>
2021-03-16 11:48:06 +0100

    working basic gui window

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,18 +2,25 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class main {
+public class main extends Application{
     private static int port = 5555;
 
     public static void main(String[] args) {
 
-        Server server = new Server(port);
-        server.startServer();
+        launch(args);
 
-        clientLoop(server);
-
-        server.stopServer();
 
     }
 
@@ -80,4 +87,62 @@ public class main {
         }
 
     }
+    @Override
+    public void start(Stage primaryStage){
+
+        Server server = new Server(port);
+        server.startServer();
+
+
+        // create a button with specified text
+        Button button = new Button("Say 'Hello World'");
+
+        // set a handler that is executed when the user activates the button
+        // e.g. by clicking it or pressing enter while it's focused
+        button.setOnAction(e -> {
+            //Open information dialog that says hello
+            Client c = new Client(port, "navn", "pass");
+            System.out.println("client: " + c.getName());
+
+        });
+
+        Label label1 = new Label("Name:");
+        TextField textField = new TextField ();
+        HBox hb = new HBox();
+        hb.getChildren().addAll(label1, textField);
+        hb.setSpacing(10);
+
+        // the root of the scene shown in the main window
+        StackPane root = new StackPane();
+
+        // add button as child of the root
+        root.getChildren().add(button);
+
+        // create a scene specifying the root and the size
+        Scene scene = new Scene(root, 500, 300);
+
+        // add scene to the stage
+        primaryStage.setScene(scene);
+
+        // make the stage visible
+        primaryStage.show();
+
+    }
+
+    /*
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+        Server server = new Server(port);
+        server.startServer();
+        startUI((primaryStage));
+
+        clientLoop(server);
+
+        server.stopServer();
+
+    }
+
+     */
 }
