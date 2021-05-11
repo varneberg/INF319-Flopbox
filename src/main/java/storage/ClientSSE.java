64b06c4b7ac78a342f5e50ac96cd1deb8adba12f
kristoffer <kristofferbn98@gmail.com>
2021-05-11 16:16:21 +0200

    added server and client sse, no functionality

---
+++ src/main/java/storage/ClientSSE.java
@@ -0,0 +1,21 @@
+package storage;
+
+public class ClientSSE {
+    private static int blockSize = 64;
+
+    public ClientSSE(String username){
+
+    }
+
+    public String generateSearchToken(String keyword){
+        return null;
+    }
+
+    public String[] decryptFile(){
+        return null;
+    }
+
+    private String decryptBlock(){
+        return null;
+    }
+}
