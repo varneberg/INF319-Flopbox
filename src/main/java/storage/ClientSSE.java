27faec66fb117b06b1b30425db0dc20f78208422
kristoffer <kristofferbn98@gmail.com>
2021-05-18 16:02:16 +0200

    started work on sse

--- src/main/java/storage/ClientSSE.java
+++ src/main/java/storage/ClientSSE.java
@@ -1,10 +1,24 @@
 package storage;
 
 public class ClientSSE {
-    private static int blockSize = 64;
+
+
+    private static int blockSize = 24;
+    private static int m = blockSize/2;
+    private String[] s_values;
+    private String username;
 
     public ClientSSE(String username){
+        this.username = username;
+        
+    }
 
+    private void generateS(){
+
+    }
+
+    private String getS(int i){
+        return null;
     }
 
     public String generateSearchToken(String keyword){
